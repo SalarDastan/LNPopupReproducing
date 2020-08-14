1fbbcd59b390dae90e38cc733ad82a433fb51300
Hossein Dastan <s.hossein.dastan@gmail.com>
2020-08-14 14:33:53 -0700

    Initial Commit

---
+++ LNPopupReproducing/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  LNPopupReproducing
+//
+//  Created by Hossein Dastan on 8/14/20.
+//  Copyright © 2020 Hossein Dastan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LNPopupReproducing/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  LNPopupReproducing
+//
+//  Created by Hossein Dastan on 8/14/20.
+//  Copyright © 2020 Hossein Dastan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
